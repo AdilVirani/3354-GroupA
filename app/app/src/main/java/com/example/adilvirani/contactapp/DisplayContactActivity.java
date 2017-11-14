cefe81468ea991fa423a7885c66b1ab7fa2f1442
ideeitgirl <evelyn.l.wong97@gmail.com>
2017-11-14 00:37:14 -0600

    Make changes to display

---
+++ app/app/src/main/java/com/example/adilvirani/contactapp/DisplayContactActivity.java
@@ -0,0 +1,13 @@
+package com.example.adilvirani.contactapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_contact);
+    }
+}
--- app/app/src/main/java/com/example/adilvirani/contactapp/MainActivity.java
+++ app/app/src/main/java/com/example/adilvirani/contactapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.adilvirani.contactapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Contact's button */
+    public void displayContactInfo(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayContactActivity.class);
+        startActivity(intent);
+    }
+
+    public void editContact(View view) {
+        Intent intent = new Intent(this, DisplayEditActivity.class);
+        startActivity(intent);
     }
 }
